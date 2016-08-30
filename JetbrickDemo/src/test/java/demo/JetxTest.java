75045f999bfa65ffc098fa59a6438c90b7b42d88
zhangzh <zhangzh@fxiaoke.com>
2016-08-30 18:20:39 +0800

    add elastic job demo

--- JetbrickDemo/src/test/java/demo/JetxTest.java
+++ JetbrickDemo/src/test/java/demo/JetxTest.java
@@ -12,19 +12,19 @@ import java.util.Map;
 
 public class JetxTest {
 
-    public static final String templateStr = "<table>\n" +
-            "  <tr>\n" +
-            "    <td>序号</td>\n" +
-            "    <td>姓名</td>\n" +
-            "    <td>邮箱</td>\n" +
-            "  </tr>\n" +
-            "  ##for (user : users)\n" +
-            "  <tr>\n" +
-            "    <td>${name}</td>\n" +
-            "    <td>${email}</td>\n" +
-            "  </tr>\n" +
-            "  ##end\n" +
-            "</table>";
+//    public static final String templateStr = "<table>\n" +
+//            "  <tr>\n" +
+//            "    <td>序号</td>\n" +
+//            "    <td>姓名</td>\n" +
+//            "    <td>邮箱</td>\n" +
+//            "  </tr>\n" +
+//            "  ##for (user : users)\n" +
+//            "  <tr>\n" +
+//            "    <td>${name}</td>\n" +
+//            "    <td>${email}</td>\n" +
+//            "  </tr>\n" +
+//            "  ##end\n" +
+//            "</table>";
 
     @Test
     public void test() {
@@ -46,7 +46,7 @@ public class JetxTest {
 //        context.put("users", users);
         context.put("name","张三");
         context.put("email","zhangsan@qq.com");
-        context.put("url","www.fxiaoke.com");
+        context.put("url","www.123.com");
 
         // 4. 渲染模板到自定义的 Writer
         StringWriter writer = new StringWriter();
---
+++ Test/src/main/java/TestCase.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zhangzh on 2016/8/23.
+ */
+public class TestCase {
+
+    public static void main(String[] args) {
+        String s = null;
+
+        if(s instanceof String) {
+            System.out.println("SS: "+s);
+        }
+    }
+}
